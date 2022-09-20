90d63bdb93e55d44601243c4db80373639066bca
RoninMirrorImage <102737912+RoninMirrorImage@users.noreply.github.com>
2022-09-20 19:25:51 +0200

    finire bottoni muto

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ import { searchClickSoundEffect } from "./searchClickSoundEffect.js";
 
 const inputEventFunc = async() => {
 
+    // searchClickSoundEffect();
+    // initBackgroundSoundTheme();
+
     document.querySelector(".searchBtn").onclick = async() => {
-        searchClickSoundEffect();
-        await searchInsidePokedex(inputValueFormat(document.querySelector("input").value));
-        document.querySelector("input").value = "";
-        readMoreBtnFunc();
+        if(document.querySelector("input").value.replaceAll(" ", "") !== "" ){
+            searchClickSoundEffect();
+            await searchInsidePokedex(inputValueFormat(document.querySelector("input").value));
+            document.querySelector("input").value = "";
+            readMoreBtnFunc();
+        }
     }
     
     document.querySelector("input").addEventListener("keypress", async (e) => {
-        if(e.key === "Enter"){
+        if(e.key === "Enter" && document.querySelector("input").value.replaceAll(" ", "") !== "" ){
             e.preventDefault();
             searchClickSoundEffect();
             await searchInsidePokedex(inputValueFormat(document.querySelector("input").value));
